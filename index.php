8602e41407a5d73ef4d3dfc49d237e4bd0f99028
Ivan Churkin <bahes.in@gmail.com>
2020-02-22 20:48:32 +0300

    feat: step-10-functions

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+require 'functions.php';
+
 $task = [
     'title' => 'Do something',
     'due' => 'today',
     'assigned_to' => 'Ivan',
     'completed' => true
 ];
+
+dd($task);
 
 require 'index.view.php';
