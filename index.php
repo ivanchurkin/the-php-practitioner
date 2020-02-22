1decccd57bb25bed535848ee277019e042281604
Ivan Churkin <bahes.in@gmail.com>
2020-02-22 15:19:47 +0300

    feat: step-7-associative-arrays

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
-$names = [
-    'Ivan',
-    'Kris',
-    'John'
+$person = [
+    'name' => 'Ivan',
+    'age' => '26',
+    'hair' => 'brown'
 ];
+
+$person['birthday'] = '6 August 1993';
+
+unset($person['hair']);
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -6,28 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <style>
-        header {
-            background-color: #ddd;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
-    <p>Variant #1</p>
     <ul>
-        <?php foreach ($names as $name) : ?>
-        <li><?= $name ?></li>
+        <?php foreach ($person as $key => $value) : ?>
+        <li><strong><?= $key ?>:</strong> <?= $value ?></li>
         <?php endforeach; ?>
-    </ul>
-    <p>Variant #2</p>
-    <ul>
-        <?php
-            foreach ($names as $name) {
-                echo "<li>$name</li>";
-            }
-        ?>
     </ul>
 </body>
 </html>
